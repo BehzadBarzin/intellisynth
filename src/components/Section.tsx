836c04d30311e6abdb47630ae09fcfe6fe67dda0
Behzad Barzin <behzad_barzin@live.com>
2024-06-23 19:35:45 +0330

    Adds Section Component

---
+++ src/components/Section.tsx
@@ -0,0 +1,55 @@
+import { FC, ReactNode } from "react";
+
+import SectionSvg from "../assets/svg/SectionSvg.jsx";
+
+interface IProps {
+  className?: string;
+  id?: string;
+  crosses?: boolean;
+  crossesOffset?: string;
+  customPaddings?: string;
+  children?: ReactNode;
+}
+
+const Section: FC<IProps> = ({
+  className,
+  id,
+  crosses,
+  crossesOffset,
+  customPaddings,
+  children,
+}) => {
+  return (
+    <div
+      id={id}
+      className={`
+      relative 
+      ${
+        customPaddings ||
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
+      } 
+      ${className || ""}`}
+    >
+      {/* Children */}
+      {children}
+
+      {/* Vertical Side Lines */}
+      <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
+      <div className="hidden absolute top-0 right-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:right-7.5 xl:right-10" />
+
+      {/* Bottom Cross Divider */}
+      {crosses && (
+        <>
+          <div
+            className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
+              crossesOffset && crossesOffset
+            } pointer-events-none lg:block xl:left-10 right-10`}
+          />
+          <SectionSvg crossesOffset={crossesOffset} />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Section;
