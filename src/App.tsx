c7f1b3152618c171aa25970094dbcca8d3b48a93
Behzad Barzin <behzad_barzin@live.com>
2024-06-23 19:59:17 +0330

    Working on Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Header from "./components/Header";
+import Hero from "./components/Hero.js";
 
 function App() {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
 
       {/* Button Gradient defines the svg gradient used in buttons and must me simply included here */}
