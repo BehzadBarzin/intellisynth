004f5b68a5cd2eb4c92b9a6653ec72d98a0d41ed
Behzad Barzin <behzad_barzin@live.com>
2024-06-24 02:11:52 +0330

    Implements Roadmap and Footer sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 import Collaboration from "./components/Collaboration.js";
 import Features from "./components/Features.js";
+import Footer from "./components/Footer.js";
 import Header from "./components/Header";
 import Hero from "./components/Hero.js";
 import Pricing from "./components/Pricing.js";
+import Roadmap from "./components/Roadmap.js";
 import Services from "./components/Services.js";
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
 
       {/* Button Gradient defines the svg gradient used in buttons and must me simply included here */}
